53267fa4b34dc0c95e07630ca93b6b3830552d0e
FloralLife <ocard9611@naver.com>
2024-11-09 16:11:41 +0900

    init

---
+++ src/test/kotlin/org/example/coffeeorder/CoffeeOrderApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.coffeeorder
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CoffeeOrderApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
